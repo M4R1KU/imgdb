d8f8728b56a4c86a09faef6b6c00b89156b8e5d6
Mario Kunz <mario99.kunz@gmail.com>
2016-05-18 21:26:20 +0200

    I don't check this shit.
    Why I do have to make 2 commits if I want to add a new file.

--- autoloader.php
+++ autoloader.php
@@ -12,28 +12,26 @@
 
 const classes = [
     "MKWeb\\ImgDB\\Controller\\Controller" => __DIR__ . "/Controller/Controller.php",
-    "MKWeb\\ImgDB\\Controller\\IndexController" => ROOT . "/Controller/IndexController.php",
-    "MKWeb\\ImgDB\\Controller\\LoginController" => ROOT . "/Controller/LoginController.php",
-    "MKWeb\\ImgDB\\Controller\\NotFoundController" => ROOT . "/Controller/NotFoundController.php",
+    "MKWeb\\ImgDB\\Controller\\IndexController" => __DIR__ . "/Controller/IndexController.php",
+    "MKWeb\\ImgDB\\Controller\\LoginController" => __DIR__ . "/Controller/LoginController.php",
+    "MKWeb\\ImgDB\\Controller\\NotFoundController" => __DIR__ . "/Controller/NotFoundController.php",
 
-    "MKWeb\\ImgDB\\Model\\Model" => ROOT . "/Model/Model.php",
-    "MKWeb\\ImgDB\\Model\\User" => ROOT . "/Model/User.php",
+    "MKWeb\\ImgDB\\Model\\Model" => __DIR__ . "/Model/Model.php",
+    "MKWeb\\ImgDB\\Model\\User" => __DIR__ . "/Model/User.php",
     //"\\MKWeb\\ImgDB\\Model\\Model" => ROOT . "/Model/Model.php"
     
-    "MKWeb\\ImgDB\\View\\View" => ROOT . "/View/View.php",
+    "MKWeb\\ImgDB\\View\\View" => __DIR__ . "/View/View.php",
     
-    "MKWeb\\ImgDB\\Network\\Request" => ROOT . "/Network/Request.php",
-    "MKWeb\\ImgDB\\Network\\Response" => ROOT . "/Network/Response.php",
+    "MKWeb\\ImgDB\\Network\\Request" => __DIR__ . "/Network/Request.php",
+    "MKWeb\\ImgDB\\Network\\Response" => __DIR__ . "/Network/Response.php",
     "MKWeb\\ImgDB\\Network\\Dispatcher" => __DIR__ . "/Network/Dispatcher.php",
-    "MKWeb\\ImgDB\\Network\\ControllerResolver" => ROOT . "/Network/ControllerResolver.php"
+    "MKWeb\\ImgDB\\Network\\ControllerResolver" => __DIR__ . "/Network/ControllerResolver.php"
 ];
 
 /**
  * @param $className
  */
 function __autoload($className) {
-    print_r($className);
-    print_r(classes);
     $map = classes;
     if (isset($map[$className])) {
         /** @noinspection PhpIncludeInspection */
